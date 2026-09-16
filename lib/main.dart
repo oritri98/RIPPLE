ff6ace979e1bdff59644beac88491ea406cf8664
oritri98 <ismatsiddiquee98@gmail.com>
2026-09-17 03:01:03 +0600

    clickable button add kora instead, Dribble used

--- lib/main.dart
+++ lib/main.dart
@@ -26,29 +26,52 @@ void main() => runApp(
                     color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),
               ),
 
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.school_outlined, color: Colors.white),
-                  Text('Student', style: TextStyle(color: Colors.white)),
-                  Icon(Icons.arrow_forward, color: Colors.white, size: 16),
-                ],
+
+
+
+               //clickable button add kora instead
+              //Dribble
+
+              OutlinedButton(
+                onPressed: () {
+
+                  print("Student button clicked!");
+                },
+                style: OutlinedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                  side: const BorderSide(color: Colors.white54, width: 1.5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: const SizedBox(
+                  width: 120,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.school_outlined, color: Colors.white),
+                      Text('Student', style: TextStyle(color: Colors.white)),
+                      Icon(Icons.arrow_forward, color: Colors.white, size: 16),
+                    ],
+                  ),
+                ),
               ),
 
-          const SizedBox(height: 30),
-          Container(
-            width: 160,
-            padding: const EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(5),
 
-            ),
-          ),
+              //ekta button add kora done
 
 
 
-            ], //children
+
+
+
+
+
+
+
+
+
+            ], //children end
           ),
         ),
       ),
