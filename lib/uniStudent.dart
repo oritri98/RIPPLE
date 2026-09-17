66a52dc94d9c50066fb40205eff1d0d55df46917
oritri98 <ismatsiddiquee98@gmail.com>
2026-09-17 06:01:23 +0600

    all files created, base code added

---
+++ lib/uniStudent.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+class uniStudentPage extends StatelessWidget {
+  const uniStudentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      // Brown color Color(0xFF1E140F) eita chosen, not Color(0xFF2C1E16)
+      backgroundColor: const Color(0xFF1E140F),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+
+
+
+          // smaller phone hoile user can scroll down and crashing/overflowing hobe na
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'What ?',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold),
+                ),
+
+
+
+
+                const SizedBox(height: 40),
+
+
+
+                //clickable button add kora instead
+
+                //code taken from https://www.geeksforgeeks.org/flutter
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("High School clicked"); },
+                  style: OutlinedButton.styleFrom(
+
+                    // button size control kora
+
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+
+
+                  //button er bhitorer text bold and size control
+
+                  child: const Text('High School',style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("University Student clicked"); },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text('University Student', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("Just Graduated clicked"); },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text('Just Graduated', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("Working clicked"); },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text('Working', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("Changing Careers clicked"); },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text('Changing Careers', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("Building My Own Thing clicked"); },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text('Building My Own Thing', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+
+
+                OutlinedButton(
+                  onPressed: () { print("Entrepreneur clicked"); },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: const Size(300, 60),
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  child: const Text('Entrepreneur', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                ),
+
+
+
+
+
+                const SizedBox(height: 30),
+
+
+
+
+                // 7 buttons add kora done (same style)
+
+              ], ////children end
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
